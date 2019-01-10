8b7ca573ec2d58314513d56bc76f343a50dcac9b
whize3 <whize3@gmail.com>
2019-01-10 20:43:27 +0900

    Initial commit

---
+++ src/main/java/com/whize/webservice/web/WebRestController.java
@@ -0,0 +1,12 @@
+package com.whize.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
