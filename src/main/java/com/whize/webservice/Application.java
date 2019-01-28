cccad06bc2e04494dd862995bdf4a3466bd25056
whize3 <whize3@gmail.com>
2019-01-28 22:38:27 +0900

    real-application.yml 읽기

--- src/main/java/com/whize/webservice/Application.java
+++ src/main/java/com/whize/webservice/Application.java
@@ -15,7 +15,6 @@ public class Application {
         new SpringApplicationBuilder(Application.class)
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
-        ;
     }
 
 }
